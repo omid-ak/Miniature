1a05bbe1967a4ffcb68e5c8a81258f3224d042a8
omid <omid7798@gmail.com>
2019-04-25 18:48:09 +0430

    save lables and duplicate error handling Done in addition some improvements

--- assembeler.py
+++ assembeler.py
@@ -1,5 +1,5 @@
 """
-this ia a assembeller known as Miniature.
+this is a assembler known as Miniature.
 It's Instructions has 3 kinds of format: R type, I type and J type
 """
 
@@ -12,7 +12,7 @@ fw = open(O_file_name, 'a')
 pc = 0
 line_counter = 0
 command_list = ['add', 'sub', 'slt', 'or', 'nand', 'addi', 'ori', ' slti', 'lui', 'lw', 'sw', 'beq', 'jalr', 'j', '.space', '.fill']
-Label_list = []
+Label_list = {}
 ############## Calculation R type Instruction ######################
 def R_Instruction(rd, rt, rs, op):
     res = ''
@@ -38,16 +38,23 @@ def Convertor_to_binary(number):
 def Convert_to_Hexa(number):
     res = hex(int(number))
     return res
+############# a function to Handle not defined labels ########################################
+def Not_define_label_Error(Label, LC):
+    if Label not in Label_list.keys():
+        print("an error occurred on line %d" % LC)
+        print("This Label Not Defined")
+        exit(1)
+##############################################################################################
+
 file_ls = fn.read().splitlines()
 for l in file_ls:
 
 #################### R Type Instructions ############################
     if 'add' in l:
         opcode_0 = '0000'
-        match_0  = re.search(r'.* add .*,*,*', str(l))
+        match_0  = re.search(r'.* add .*,.*,.*', str(l))
         argsls_0 = match_0.group().split()
         argsls_0[2] = argsls_0[2].split(',')
-        Label_list.append(argsls_0[0])
         rd_0 = Convertor_to_binary(int(argsls_0[2][0]))
         rt_0 = Convertor_to_binary(int(argsls_0[2][1]))
         rs_0 = Convertor_to_binary(int(argsls_0[2][2]))
@@ -58,12 +65,17 @@ for l in file_ls:
         fw.write(result_str_0)
         pc += 1
         line_counter += 1
+        if argsls_0[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_0[0] != "-":
+            Label_list[argsls_0[0]] = line_counter
     elif 'sub' in l:
         opcode_1 = '0001'
         match_1  = re.search(r'.* sub .*,.*,.*', str(l))
         argsls_1 = match_1.group().split()
         argsls_1[2] = argsls_1[2].split(',')
-        Label_list.append(argsls_1[0])
         rd_1 = Convertor_to_binary(int(argsls_1[2][0]))
         rt_1 = Convertor_to_binary(int(argsls_1[2][1]))
         rs_1 = Convertor_to_binary(int(argsls_1[2][2]))
@@ -74,12 +86,17 @@ for l in file_ls:
         fw.write(result_str_1)
         pc += 1
         line_counter += 1
+        if argsls_1[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_1[0] != "-":
+            Label_list[argsls_1[0]] = line_counter
     elif 'slt' in l:
         opcode_2 = '0010'
         match_2  = re.search(r'.* slt .*,.*,.*', str(l))
-        argsls_2 = match_2.group().split()              #L,slt,$rd,$rs,$rt  and ignore comments
+        argsls_2 = match_2.group().split()
         argsls_2[2] = argsls_2[2].split(',')
-        Label_list.append(argsls_2[0])
         rd_2 = Convertor_to_binary(int(argsls_2[2][0]))
         rt_2 = Convertor_to_binary(int(argsls_2[2][1]))
         rs_2 = Convertor_to_binary(int(argsls_2[2][2]))
@@ -90,12 +107,17 @@ for l in file_ls:
         fw.write(result_str_2)
         pc += 1
         line_counter += 1
+        if argsls_2[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_2[0] != "-":
+            Label_list[argsls_2[0]] = line_counter
     elif 'or' in l:
         opcode_3 = '0011'
         match_3  = re.search(r'.* or .*,.*,.*', str(l))
         argsls_3 = match_3.group().split()
         argsls_3[2] = argsls_3[2].split(',')
-        Label_list.append(argsls_3[0])
         rd_3 = Convertor_to_binary(int(argsls_3[2][0]))
         rt_3 = Convertor_to_binary(int(argsls_3[2][1]))
         rs_3 = Convertor_to_binary(int(argsls_3[2][2]))
@@ -106,12 +128,17 @@ for l in file_ls:
         fw.write(result_str_3)
         pc += 1
         line_counter += 1
+        if argsls_3[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_3[0] != "-":
+            Label_list[argsls_3[0]] = line_counter
     elif 'nand' in l:
         opcode_4 = '0100'
         match_4  = re.search(r'.* nand .*,.*,.*', str(l))
         argsls_4 = match_4.group().split()
         argsls_4[2] = argsls_4[2].split(',')
-        Label_list.append(argsls_4[0])
         rd_4 = Convertor_to_binary(int(argsls_4[2][0]))
         rt_4 = Convertor_to_binary(int(argsls_4[2][1]))
         rs_4 = Convertor_to_binary(int(argsls_4[2][2]))
@@ -122,13 +149,18 @@ for l in file_ls:
         fw.write(result_str_4)
         pc += 1
         line_counter += 1
+        if argsls_4[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_4[0] != "-":
+            Label_list[argsls_4[0]] = line_counter
 ####################### I Type Instructions ############################
     elif 'addi' in l:
         opcode_5 = '0101'
         match_5  = re.search(r'.* addi .*,.*,.*', str(l))
         argsls_5 = match_5.group().split()
         argsls_5[2] = argsls_5[2].split(',')
-        Label_list.append(argsls_5[0])
         rt_5  = Convertor_to_binary(int(argsls_5[2][0]))
         rs_5  = Convertor_to_binary(int(argsls_5[2][1]))
         imm_5 = Convertor_to_binary(int(argsls_5[2][2]))
@@ -139,12 +171,17 @@ for l in file_ls:
         fw.write(result_str_5)
         pc += 1
         line_counter += 1
+        if argsls_5[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_5[0] != "-":
+            Label_list[argsls_5[0]] = line_counter
     elif 'slti' in l:
         opcode_6 = '0110'
         match_6  = re.search(r'.* slti .*,.*,.*', str(l))
         argsls_6 = match_6.group().split()
         argsls_6[2] = argsls_6[2].split(',')
-        Label_list.append(argsls_6[0])
         rt_6 = Convertor_to_binary(int(argsls_6[2][0]))
         rs_6 = Convertor_to_binary(int(argsls_6[2][1]))
         imm_6 = Convertor_to_binary(int(argsls_6[2][2]))
@@ -155,12 +192,17 @@ for l in file_ls:
         fw.write(result_str_6)
         pc += 1
         line_counter += 1
+        if argsls_6[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_6[0] != "-":
+            Label_list[argsls_6[0]] = line_counter
     elif 'ori' in l:
         opcode_7 = '0111'
         match_7  = re.search(r'.* ori .*,.*,.*', str(l))
         argsls_7 = match_7.group().split()
         argsls_7[2] = argsls_7[2].split(',')
-        Label_list.append(argsls_7[0])
         rt_7 = Convertor_to_binary(int(argsls_7[2][0]))
         rs_7 = Convertor_to_binary(int(argsls_7[2][1]))
         imm_7 = Convertor_to_binary(int(argsls_7[2][2]))
@@ -171,12 +213,17 @@ for l in file_ls:
         fw.write(result_str_7)
         pc += 1
         line_counter += 1
+        if argsls_7[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_7[0] != "-":
+            Label_list[argsls_7[0]] = line_counter
     elif 'lui' in l:
         opcode_8 = '1000'
         match_8  = re.search(r'.* lui .*,.*,.*', str(l))
         argsls_8 = match_8.group().split()
         argsls_8[2] = argsls_8[2].split(',')
-        Label_list.append(argsls_8[0])
         rt_8 = Convertor_to_binary(int(argsls_8[2][0]))
         rs_8 = Convertor_to_binary(int(argsls_8[2][1]))
         imm_8 = Convertor_to_binary(int(argsls_8[2][2]))
@@ -187,12 +234,17 @@ for l in file_ls:
         fw.write(result_str_8)
         pc += 1
         line_counter += 1
+        if argsls_8[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_8[0] != "-":
+            Label_list[argsls_8[0]] = line_counter
     elif 'lw' in l:
         opcode_9 = '1001'
         match_9  = re.search(r'.* lw .*,.*,.*', str(l))
         argsls_9 = match_9.group().split()
         argsls_9[2] = argsls_9[2].split(',')
-        Label_list.append(argsls_9[0])
         rt_9 = Convertor_to_binary(int(argsls_9[2][0]))
         rs_9 = Convertor_to_binary(int(argsls_9[2][1]))
         offset_9 = Convertor_to_binary(int(argsls_9[2][2]))
@@ -204,12 +256,17 @@ for l in file_ls:
         fw.write(result_str_9)
         pc += 1
         line_counter += 1
+        if argsls_9[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_9[0] != "-":
+            Label_list[argsls_9[0]] = line_counter
     elif 'sw' in l:
         opcode_10 = '1010'
         match_10  = re.search(r'.* sw .*,.*,.*', str(l))
         argsls_10 = match_10.group().split()
         argsls_10[2] = argsls_10[2].split(',')
-        Label_list.append(argsls_10[0])
         rt_10 = Convertor_to_binary(int(argsls_10[2][0]))
         rs_10 = Convertor_to_binary(int(argsls_10[2][1]))
         offset_10 = Convertor_to_binary(int(argsls_10[2][2]))
@@ -220,12 +277,17 @@ for l in file_ls:
         fw.write(result_str_10)
         pc += 1
         line_counter += 1
+        if argsls_10[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_10[0] != "-":
+            Label_list[argsls_10[0]] = line_counter
     elif 'beq' in l:
         opcode_11 = '1011'
         match_11  = re.search(r'.* beq .*,.*,.*', str(l))
         argsls_11 = match_11.group().split()
         argsls_11[2] = argsls_11[2].split(',')
-        Label_list.append(argsls_11[0])
         rt_11 = Convertor_to_binary(int(argsls_11[2][0]))
         rs_11 = Convertor_to_binary(int(argsls_11[2][1]))
         offset_11 = Convertor_to_binary(int(argsls_11[2][2]))
@@ -236,11 +298,16 @@ for l in file_ls:
         fw.write(result_str_11)
         pc += 1
         line_counter += 1
+        if argsls_11[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_11[0] != "-":
+            Label_list[argsls_11[0]] = line_counter
     elif 'jalr' in l:
         opcode_12 = '1100'
         match_12  = re.search(r'.* jalr .*,.*', str(l))
         argsls_12 = match_12.group().split()
-        Label_list.append(argsls_12[0])
         rt_12 = Convertor_to_binary(int(argsls_12[2][0]))
         rs_12 = Convertor_to_binary(int(argsls_12[2][1]))
         offset_12 = ''
@@ -251,18 +318,29 @@ for l in file_ls:
         fw.write(result_str_12)
         pc += 1
         line_counter += 1
+        if argsls_12[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_12[0] != "-":
+            Label_list[argsls_12[0]] = line_counter
 ############### J Type Instructions ##############################
     elif 'j' in l:
         opcode_13 = '1101'
         match_13  = re.search(r'.* j .*',str(l))
         argsls_13 = match_13.group().split()
-        Label_list.append(argsls_13[0])
         targ_13 = Convertor_to_binary(int(argsls_13[2]))
         binary_code_address_13 = J_Instruction(targ_13, opcode_13)
         decima_code_address_13 = int(binary_code_address_13, 2)
         hexa_code_address_13   = Convert_to_Hexa(decima_code_address_13)
         pc += 1
         line_counter += 1
+        if argsls_13[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_13[0] != "-":
+            Label_list[argsls_13[0]] = line_counter
     elif 'halt' in l:
         opcode_14 = '1110'
         A = ''
@@ -272,7 +350,7 @@ for l in file_ls:
         result_str_14 = "BINARY : %s  DECIMAL : %s  HEXADECIMAL : %s \n" % (binary_code_address_14, decima_code_address_14, hexa_code_address_14)
         fw.write(result_str_14)
         pc += 1
-        line_counter+=1
+        line_counter += 1
         exit(0)
 ############### Directive Instructions ############################
     elif '.fill' in l:
@@ -285,6 +363,12 @@ for l in file_ls:
         fw.write(result_str_15)
         pc += 1
         line_counter += 1
+        if argsls_15[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_15[0] != "-":
+            Label_list[argsls_15[0]] = line_counter
     elif '.space' in  l:
         match_16 = re.search(r'.* .space .*', str(l))
         argsls_16 = match_16.group().split()
@@ -296,18 +380,24 @@ for l in file_ls:
         fw.write(result_str_16)
         pc += 1
         line_counter += 1
+        if argsls_16[0] in Label_list.keys():
+            print("an error occurred on line %d" % line_counter)
+            print("Duplicate Label Error")
+            exit(1)
+        if argsls_16[0] != "-":
+            Label_list[argsls_16[0]] = line_counter
 ############## Denied Instructions #################################
     else:
         line_counter += 1
-        wrong_command = re.search(r'.* .*', str(l))
+        wrong_command = re.search(r'.* .* .*', str(l))
         wrong_command_ls = wrong_command.group().split()
         if wrong_command_ls[1] not in command_list:
-            print("an error has occurred in line %d" % line_counter)
-            print("miniature: %s : command not found : %s" % (l, wrong_command_ls[1]))
+            print("an error occurred on line %d" % line_counter)
+            print("miniature: command not found : %s" % wrong_command_ls[1])
         exit(1)
 
 fw.close()
 #############################################################################################
 
-#TODO: Hanlde Errors in Instructions and invalid instruction,Labels and offsets
+#TODO: Hanlde Errors in  offsets
 #TODO: Jump to lines by line_counter
